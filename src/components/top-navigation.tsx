17780fab6db0ffb371c824114c6c677f021ff24b
Leonardo (isaiahscape) <lqlp0011@gmail.com>
2025-04-15 15:23:30 +0000

    top animation: add hovers

--- src/components/top-navigation.tsx
+++ src/components/top-navigation.tsx
@@ -2,6 +2,7 @@
 
 import {Github, Linkedin, Mail} from 'lucide-react';
 import {ThemeToggle} from '@/components/theme-provider';
+import {motion} from 'framer-motion';
 
 const socialMedia = {
   email: 'isaiahscape@duck.com',
@@ -11,28 +12,34 @@ const socialMedia = {
 
 export function TopNavigation() {
   return (
-    <nav className="flex items-center mb-8">
+    <nav className="flex items-center mb-8 justify-center">
       <div className="text-xl font-bold mr-auto">Leonardo</div>
       <div className="flex items-center space-x-4 justify-end ">
-        <a href={`mailto:${socialMedia.email}`} className="hover:text-primary">
+        <motion.a
+          href={`mailto:${socialMedia.email}`}
+          className="hover:text-primary"
+          whileHover={{scale: 1.1, transition: {duration: 0.2}}}
+        >
           <Mail className="h-5 w-5" />
-        </a>
-        <a
+        </motion.a>
+        <motion.a
           href={socialMedia.github}
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-primary"
+          whileHover={{scale: 1.1, transition: {duration: 0.2}}}
         >
           <Github className="h-5 w-5" />
-        </a>
-        <a
+        </motion.a>
+        <motion.a
           href={socialMedia.linkedin}
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-primary"
+          whileHover={{scale: 1.1, transition: {duration: 0.2}}}
         >
           <Linkedin className="h-5 w-5" />
-        </a>
+        </motion.a>
         <ThemeToggle />
       </div>
     </nav>
