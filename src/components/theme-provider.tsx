e0f447cf9fe905ca56fe0f54b4cd3797ab032f28
Leonardo (isaiahscape) <lqlp0011@gmail.com>
2025-04-15 09:08:28 +0000

    pages: add animation while switching light and dark mode

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -6,6 +6,7 @@ import {type ThemeProviderProps} from 'next-themes/dist/types';
 import {Button} from '@/components/ui/button';
 import {Moon, Sun} from 'lucide-react';
 import {useTheme} from 'next-themes';
+import {motion} from 'framer-motion';
 
 export function ThemeProvider({children, ...props}: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
@@ -20,8 +21,22 @@ export function ThemeToggle() {
       size="icon"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <motion.div
+        initial={{opacity: 1, rotate: 0}}
+        animate={{opacity: 1, rotate: theme === 'dark' ? 180 : 0}}
+        transition={{duration: 0.2}}
+        style={{position: 'absolute'}}
+      >
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      </motion.div>
+      <motion.div
+        initial={{opacity: 0, rotate: theme === 'dark' ? 180 : 0}}
+        animate={{opacity: 1, rotate: theme === 'dark' ? 0 : 180}}
+        transition={{duration: 0.2}}
+        style={{position: 'absolute'}}
+      >
+        <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      </motion.div>
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
